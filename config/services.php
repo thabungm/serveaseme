d4d134a19f646539b40801647f489d9cd6e2c935
Thabungba M <thabungba.meetei@costrategix.com>
2016-02-14 10:52:33 +0530

    made fb-redirect env variable

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
     'client_id' => '1680554402218615',
     'client_secret' => 'd323f6b4acaee642792bd31ae37f8d3c',
-    'redirect' => 'http://serveaseme.local/index.php/authenticate/callback/facebook',
+    'redirect' => env('FB_REDIRECT'),
     ],
     'google' => [
     'client_id' => 'your-github-app-id',
