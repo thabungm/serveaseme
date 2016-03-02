1550f3195fe36e271514888a24819fc6b3741582
Thabungba M <thabungba.meetei@costrategix.com>
2016-03-03 02:11:48 +0530

    fixed env var

--- public/app/config.js
+++ public/app/config.js
@@ -5,7 +5,7 @@ var STATUS_COLORS = {new:"label label-info",processing:"label btn-primary btn-xs
 switch (SERV_ENV) {
     case 'production':
         API_ENGINE_URL = 'http://localhost/serveaseme/index.php/api/';
-               API_ENGINE_URL = 'http://serveaseme.local/index.php/api/';
+               API_ENGINE_URL = 'http://52.26.135.69/index.php/api/';
 
         LOGIN_FACEBOOK_LINK = 'http://serveaseme.local/index.php/auth/facebook';
         break;
